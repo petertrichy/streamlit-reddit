2dba7f87bff1729e4430bb0fbf345a1f50a6c1e4
Peter <petertrichy@gmail.com>
2021-06-05 14:01:58 +0530

    firestore created

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,19 @@
 import streamlit as st
+from google.cloud import firestore
+
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firestorekey.json")
+
+# Create a reference to the Google post.
+doc_ref = db.collection("posts").document("Google")
+
+# Then get the data at that reference.
+doc = doc_ref.get()
+
+# Let's see what we got!
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
+
 
 st.header('Hello World!')
 if st.button('Balloons?'):
