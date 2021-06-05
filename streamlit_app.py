4ad7bf6251879f5fbd0e250de0dda1f074700817
Peter <petertrichy@gmail.com>
2021-06-05 15:00:02 +0530

    firestore document 2 created

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,24 @@ doc = doc_ref.get()
 st.write("The id is: ", doc.id)
 st.write("The contents are: ", doc.to_dict())
 
+# This time, we're creating a NEW post reference for Apple
+doc_ref = db.collection("posts").document("Apple")
+
+# And then uploading some data to that reference
+doc_ref.set({
+    "title": "Apple",
+    "url": "www.apple.com"
+})
+
+# Now let's make a reference to ALL of the posts
+posts_ref = db.collection("posts")
+
+# For a reference to a collection, we use .stream() instead of .get()
+for doc in posts_ref.stream():
+    st.write("The id is: ", doc.id)
+    st.write("The contents are: ", doc.to_dict())
+
+
 
 st.header('Hello World!')
 if st.button('Balloons?'):
